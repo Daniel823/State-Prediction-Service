22d454c618696cf6bcfd4ce007e45fad03072681
Daniel Glownia <dglownia222@gmail.com>
2017-08-26 16:12:01 -0500

    add changes

--- curtain_scheduler_app.py
+++ curtain_scheduler_app.py
@@ -12,18 +12,25 @@
 
 # load model and predict
 
-import observer as curtainObserver
 import os
+from datetime import datetime
+import observer as curtainObserver
 from sklearn.externals import joblib
 
 URL = 'http://192.168.1.119:1234'
 CURRENT_DIR = 'curtain-prediction-app'
 MODEL_DIR = 'model-generator/model/model.pkl'
+NOW = datetime.now()
 
 dir_path = os.path.dirname(os.path.realpath(__file__)).replace(CURRENT_DIR, MODEL_DIR)
 
-print(dir_path)
+print('Loading model from: {}'.format(dir_path))
 clf = joblib.load(dir_path)
 
-print(curtainObserver.getState(URL))
-print(curtainObserver.updateState(URL, 0))
+#Note: you are assuming that the place where this is running has the same time as the client
+attribute  = [NOW.day, NOW.hour, NOW.minute]
+
+print('Predicting state day:{} hour:{} min:{}'.format(NOW.day, NOW.hour, NOW.minute))
+print(clf.predict([6,8,30]))
+#print(curtainObserver.getState(URL))
+#print(curtainObserver.updateState(URL, 0))
